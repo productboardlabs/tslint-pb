788f5709c0c236b3c3ee21c4c32a3caa15773e97
Jakub Benes <jukben@gmail.com>
2019-01-17 10:32:59 +0100

    Added credits to the test-script

--- scripts/test-rules.sh
+++ scripts/test-rules.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# borrowed from https://github.com/palantir/tslint-react/blob/master/scripts/test-rules :)
+
 ret=0;
 for path in `find test -name tslint.json`; do
     ./node_modules/.bin/tslint -r ./build/rules/ --test $path/..
