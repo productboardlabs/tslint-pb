d4073442304ae91a07ba200590c3a20f488d37d8
Jakub Beneš <jukben@gmail.com>
2019-05-01 11:41:39 +0200

    Allow recursive selectors and correctly watch for Identifier and PropertyAccessExpression (#38)
    * Allow recursive selectors
    
    * Correctly watch for Identifier and PropertyAccessExpression

--- src/rules/checkUnusedFluxDependenciesRule.ts
+++ src/rules/checkUnusedFluxDependenciesRule.ts
@@ -112,15 +112,23 @@ class NoUnusedDependenciesWalker extends Lint.RuleWalker {
     const [dependencyNode, implementationNode] = node.arguments;
 
     if (
-      dependencyNode.kind === ts.SyntaxKind.ArrayLiteralExpression &&
-      implementationNode.kind === ts.SyntaxKind.ArrowFunction
+      ts.isArrowFunction(implementationNode) &&
+      ts.isArrayLiteralExpression(dependencyNode)
     ) {
-      const selectorsNodesIdentifiers = (dependencyNode as ts.ArrayLiteralExpression).elements.filter(
-        a => a.kind === ts.SyntaxKind.Identifier
-      ) as Array<ts.Identifier>;
+      const selectorsNodesIdentifiers = dependencyNode.elements.filter(
+        ts.isIdentifier
+      );
+
       const dependencies = selectorsNodesIdentifiers.map(
         element => element.text
       );
+
+      if (
+        ts.isVariableDeclaration(node.parent) &&
+        ts.isIdentifier(node.parent.name)
+      ) {
+        dependencies.push(node.parent.name.text);
+      }
 
       const usedDependencies: Array<string> = [];
 
@@ -150,10 +158,20 @@ class NoUnusedDependenciesWalker extends Lint.RuleWalker {
 
       const cb = (startNode: ts.Node): void => {
         if (
-          startNode.kind === ts.SyntaxKind.Identifier &&
-          startNode.parent.kind !== ts.SyntaxKind.TypeReference
+          ts.isIdentifier(startNode) &&
+          !ts.isTypeReferenceNode(startNode.parent) &&
+          !ts.isPropertyAccessExpression(startNode.parent)
         ) {
-          const identifier = (startNode as ts.Identifier).text;
+          const identifier = startNode.text;
+
+          addToUsed(startNode, identifier);
+        }
+
+        if (
+          ts.isPropertyAccessExpression(startNode) &&
+          ts.isIdentifier(startNode.expression)
+        ) {
+          const identifier = startNode.expression.text;
 
           addToUsed(startNode, identifier);
         }
@@ -169,9 +187,9 @@ class NoUnusedDependenciesWalker extends Lint.RuleWalker {
       );
 
       unusedDependencies.forEach(unusedSelector => {
-        const unusedDependenciesNode = (dependencyNode as ts.ArrayLiteralExpression).elements.find(
-          element => (element as ts.Identifier).text === unusedSelector
-        );
+        const unusedDependenciesNode = dependencyNode.elements
+          .filter(ts.isIdentifier)
+          .find(element => element.text === unusedSelector);
 
         if (unusedDependenciesNode) {
           this.addIssue(
